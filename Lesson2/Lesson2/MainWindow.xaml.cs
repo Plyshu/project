a208ab610ba1d419342025d0bd51edfc819322a9
unknown <buzalov@KURSIZ-05.stud.vvsu.ru>
2017-03-11 16:17:56 +1000

    no message

---
+++ Lesson2/Lesson2/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Lesson2
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
